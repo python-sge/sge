71d6c64d4a8681906e0f1ebf3658c61ac7531b1e
onpon4 <onpon4@riseup.net>
2013-06-15 20:44:42 -0400

    Changed the way the READMEs are written.

--- sge-template/__init__.py
+++ sge-template/__init__.py
@@ -231,7 +231,20 @@ Global variables:
 Implementation-Specific Information
 ===================================
 
-[insert info here]
+License
+-------
+
+[insert license info here]
+
+Dependencies
+------------
+
+- Python [insert Python version here] <http://www.python.org>
+- [insert additional dependency here] <[insert dependency link here]>
+- [insert additional dependency here] <[insert dependency link here]>
+- [...]
+
+[insert other info here]
 
 """
 
