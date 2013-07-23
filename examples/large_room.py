89f107c1a4356a30e13a2eec1370d07f23e23b17
onpon4 <onpon4@riseup.net>
2013-07-22 20:24:48 -0400

    Released all the examples to the public domain with CC0.

--- examples/large_room.py
+++ examples/large_room.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-# Copyright 2012, 2013 Julian Marchant <onpon4@lavabit.com>
+# Large Room Example
+# Written in 2012, 2013 by Julian Marchant <onpon4@lavabit.com>
 #
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 """Split-screen Example
 
