82a8b7c464b2678a0bd510747237fa5b77c23a66
Julian <onpon4@riseup.net>
2013-11-05 10:20:04 -0500

    Simplified setup.py.
    Different implementations are going to be packaged separately from
    now on, so there's no need for setup.py to choose an implementation.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf8 -*-
 
 # SGE setup.py
@@ -29,13 +29,13 @@ import sys
 from distutils.core import setup
 
 long_description = """
-Stellar Game Engine (SGE) is a free, easy-to-use 2-D game engine for Python.
-
-SGE is designed to easily allow multiple implementations. Currently, only a Pygame implementation is available.
+The Stellar Game Engine (abbreviated “SGE”, pronounced as “Sage”) is a
+free 2-D game engine. The purpose of the SGE is to make game development
+easier, which allows more rapid development by experienced game
+developers and also helps less experienced game developers learn how to
+develop games.
 """.strip()
-py3_input = input if sys.version_info.major >= 3 else raw_input
 
-# Defaults
 name = "SGE"
 author = "Julian Marchant"
 author_email = "onpon4@riseup.net"
@@ -43,77 +43,6 @@ url = "http://stellarengine.nongnu.org"
 license = "CC0"
 idir = "sge"
 requires = []
-
-def choose_lib(libs_available):
-    """Return the library (as a string) to use.
-
-    Arguments:
-
-    - ``libs_available`` -- A list or tuple of the possible libraries to
-      choose from.
-
-    """
-    if not libs_available:
-        return None
-    elif len(libs_available) == 1:
-        return 0
-    else:
-        print("Please choose one of the following libraries to use for SGE:")
-        
-        i = 0
-        for lib in libs_available:
-            print("{0} ({1})".format(lib, i))
-            i += 1
-
-        choice = py3_input("Enter the number of your choice [0] ").strip()
-        
-        if not choice:
-            choice = "0"
-        
-        while not (choice.isdigit() and 0 <= choice < len(libs_available)):
-            print("Invalid entry; must be an integer from 0 to {0}.".format(
-                len(libs_available)))
-            choice = py3_input("Enter the number of your choice [0] ").strip()
-
-            if not choice:
-                choice = "0"
-
-        return choice
-
-if sys.version_info.major == 3:
-    libs_available = []
-    try:
-        import pygame
-        if pygame.version.vernum >= (1, 9):
-            libs_available.append('pygame')
-    except ImportError:
-        pass
-
-    choice = choose_lib(libs_available)
-
-    if choice == 'pygame':
-        name = 'SGE Pygame'
-        license = "GNU Lesser General Public License version 3 or later"
-        idir = 'sge-pygame-py3k'
-        requires = ['pygame (>= 1.9.0)']
-elif sys.version_info.major == 2 and sys.version_info.minor >= 6:
-    libs_available = []
-    try:
-        import pygame
-        if pygame.version.vernum >= (1, 9):
-            libs_available.append('pygame')
-    except ImportError:
-        pass
-
-    choice = choose_lib(libs_available)
-
-    if choice == 'pygame':
-        name = 'SGE Pygame'
-        license = "GNU Lesser General Public License version 3 or later"
-        idir = 'sge-pygame'
-        requires = ['pygame (>= 1.9.0)']
-else:
-    sys.exit('Python version unsupported.')
 
 setup(name=name,
       version='0.4.0-dev',
