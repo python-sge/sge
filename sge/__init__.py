8d9e92e7ebfd970caa68443c9522ec68ccceb578
onpon4 <onpon4@riseup.net>
2015-02-02 14:14:36 -0500

    Suggest __future__ imports in the spec.

--- sge/__init__.py
+++ sge/__init__.py
@@ -243,6 +243,11 @@ Dependencies
 [insert other info here]
 """
 
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
 __version__ = "0.16a0"
 
 import sys
