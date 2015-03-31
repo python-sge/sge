c173214691fcb518ebf2eb4e7050267603150eaf
onpon4 <onpon4@riseup.net>
2015-03-31 11:59:15 -0400

    0.18a0

--- sge/__init__.py
+++ sge/__init__.py
@@ -248,7 +248,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.17"
+__version__ = "0.18a0"
 
 import sys
 import os
