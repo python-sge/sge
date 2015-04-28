d3bc1d789f76c6d3bf8e955ecf54e4f8319f3219
onpon4 <onpon4@riseup.net>
2015-04-28 13:42:24 -0400

    SGE spec 0.18 release

--- sge/__init__.py
+++ sge/__init__.py
@@ -248,7 +248,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.18a0"
+__version__ = "0.18"
 
 import sys
 import os
