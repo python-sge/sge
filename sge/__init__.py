94fb2b7892c76759e5c5b75d442ce04c43f8c29a
onpon4 <onpon4@riseup.net>
2015-06-08 18:46:58 -0400

    Fixed version number.
    Forgot the a0.

--- sge/__init__.py
+++ sge/__init__.py
@@ -248,7 +248,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.19"
+__version__ = "0.19a0"
 
 import sys
 import os
