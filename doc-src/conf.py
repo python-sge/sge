3a4ad58947dbdd20609602dc052bc7b88f6c702b
onpon4 <onpon4@riseup.net>
2015-08-23 21:30:00 -0400

    Added a favicon.

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -121,12 +121,12 @@ html_theme = 'nature'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "sge_logo_alpha_black.png"
+html_logo = "_static/sge_logo_alpha_black.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
