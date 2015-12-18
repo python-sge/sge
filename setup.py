a58ac0ea1c99da575b7c5c05d2a602bc446bbe3b
onpon4 <onpon4@riseup.net>
2015-12-17 20:15:47 -0500

    Release 0.22.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ This implementation of the SGE uses Pygame as a backend.
 """.strip()
 
 setup(name="sge-pygame",
-      version="0.22a0",
+      version="0.22",
       description="A 2-D game engine for Python",
       long_description=long_description,
       author="Julian Marchant",
--- sge/__init__.py
+++ sge/__init__.py
@@ -337,7 +337,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.22a0"
+__version__ = "0.22"
 
 import sys
 import os
