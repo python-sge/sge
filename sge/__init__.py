1dc80b0be29f43d3e2d61545cdec18610267983e
onpon4 <onpon4@riseup.net>
2016-09-07 00:07:45 -0400

    Pygame SGE 1.1.1

--- sge/__init__.py
+++ sge/__init__.py
@@ -235,7 +235,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "1.1.1a0"
+__version__ = "1.1.1"
 
 import sys
 import os
