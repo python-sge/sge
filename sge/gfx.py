python-sge/sge

commit c08ad47254ce46293cd090ee6ea34c04ae029925

Made color to int conversion code clearer.

sge/gfx.py
@@ -186,7 +186,7 @@ class Color:
         return iter([self.red, self.green, self.blue, self.alpha])
 
     def __int__(self):
-        return self.red * 256 ** 2 | self.green * 256 | self.blue
+        return self.red << 2 | self.green << 1 | self.blue
 
     def __repr__(self):
         return 'sge.gfx.Color("{}")'.format(str(self))

commit bf23e0fc2530a2709c0bf8cb21e4dd6d441f4a9c

I am so, so embarrassed…
c08ad47254ce46293cd090ee6ea34c04ae029925 broke conversion of color
objects to integers. This commit fixes that.

sge/gfx.py
@@ -198,7 +198,7 @@ class Color:
         return iter([self.red, self.green, self.blue, self.alpha])
 
     def __int__(self):
-        return self.red << 2 | self.green << 1 | self.blue
+        return self.red << 16 | self.green << 8 | self.blue
 
     def __repr__(self):
         return 'sge.gfx.Color("{}")'.format(str(self))